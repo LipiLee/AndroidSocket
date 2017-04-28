fc3477ecf32dbb96496621a095ffdcc7425cdf98
Lipi Lee <lipisoft@gmail.com>
2017-04-28 16:14:26 +0900

    Handle the socket command from a main activity in a running thread with a handler.

--- app/src/main/java/com/lipisoft/mysocket/MainActivity.java
+++ app/src/main/java/com/lipisoft/mysocket/MainActivity.java
@@ -1,12 +1,19 @@
 package com.lipisoft.mysocket;
 
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int CONNECT = 0;
+    public static final int WRITE = 1;
+    public static final int READ = 2;
+    public static final int CLOSE = 3;
+
+    private boolean isBlockingMode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,29 +24,32 @@ public class MainActivity extends AppCompatActivity {
 
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton v, boolean isChecked) {
-                MySocketManager.getInstance().setBlockingMode(isChecked);
+//                MySocketManager.getInstance().setBlockingMode(isChecked);
+                isBlockingMode = isChecked;
             }
         });
 
-    }
-
-    public void onConnectThread(View v) {
-        final Thread myThread = new Thread(new ConnectRunnable());
+        final Thread myThread = new Thread(new SocketRunnable());
         myThread.start();
     }
 
-    public void onWriteThread(View v) {
-        final Thread myThread = new Thread(new WriteRunnable());
-        myThread.start();
+    public void onConnect(View v) {
+        final Message message = SocketRunnable.handler.obtainMessage(CONNECT, isBlockingMode);
+        message.sendToTarget();
     }
 
-    public void onReadThread(View v) {
-        final Thread myThread = new Thread(new ReadRunnable());
-        myThread.start();
+    public void onWrite(View v) {
+        final Message message = SocketRunnable.handler.obtainMessage(WRITE, isBlockingMode);
+        message.sendToTarget();
     }
 
-    public void onCloseThread(View v) {
-        final Thread myThread = new Thread(new CloseRunnable());
-        myThread.start();
+    public void onRead(View v) {
+        final Message message = SocketRunnable.handler.obtainMessage(READ, isBlockingMode);
+        message.sendToTarget();
+    }
+
+    public void onClose(View v) {
+        final Message message = SocketRunnable.handler.obtainMessage(CLOSE, isBlockingMode);
+        message.sendToTarget();
     }
 }
---
+++ app/src/main/java/com/lipisoft/mysocket/SocketRunnable.java
@@ -0,0 +1,125 @@
+package com.lipisoft.mysocket;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
+class SocketRunnable implements Runnable {
+    private static final String TAG = "SocketRunnable";
+    static Handler handler;
+    private SocketChannel channel;
+
+    @Override
+    public void run() {
+        Looper.prepare();
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                Boolean blockingMode = (Boolean) msg.obj;
+
+                switch (msg.what) {
+                    case MainActivity.CONNECT:
+                        connect(blockingMode);
+                        break;
+                    case MainActivity.WRITE:
+                        write();
+                        break;
+                    case MainActivity.READ:
+                        read();
+                        break;
+                    case MainActivity.CLOSE:
+                        close();
+                        break;
+                }
+            }
+        };
+        Looper.loop();
+    }
+
+    private void connect(boolean blockingMode) {
+        final SocketAddress socket = new InetSocketAddress("m.play.melon.com", 80);
+
+        try {
+            channel = SocketChannel.open(socket);
+            channel.configureBlocking(blockingMode);
+        } catch (IOException e) {
+            Log.e(TAG, "failed to connect.");
+        }
+    }
+
+    private void write() {
+        if (channel != null) {
+            final String request = "GET / HTTP/1.1\r\n";
+            final String userAgent = "User-Agent: Android 6.0.1; SM-G900S\r\n";
+            final String host = "Host: m.play.melon.com\r\n";
+//            final String connectionMode = "Connection: Keep-Alive\r\n";
+            final String end = "\r\n";
+
+//            final String message = request + userAgent + host + connectionMode + end;
+            final String httpHeader = request + userAgent + host + end;
+
+
+            final ByteBuffer httpGet = ByteBuffer.allocate(2048);
+            httpGet.put(httpHeader.getBytes());
+            httpGet.flip();
+
+            try {
+                channel.write(httpGet);
+            } catch (IOException e) {
+                Log.e(TAG, "failed to write.", e);
+            }
+        } else {
+            Log.e(TAG, "SocketChannel is not valid.");
+        }
+    }
+
+    private void read() {
+        if (channel != null) {
+            final ByteBuffer resHttp = ByteBuffer.allocate(2048);
+            try {
+                int numberReceived = channel.read(resHttp);
+
+                while (numberReceived > 0) {
+                    resHttp.flip();
+
+                    final byte[] response = new byte[numberReceived];
+                    resHttp.get(response);
+
+                    Log.d(TAG, new String(response));
+
+                    resHttp.clear();
+                    numberReceived = channel.read(resHttp);
+                }
+                Log.d(TAG, "Received Bytes: " + numberReceived);
+
+                // When the remote server sends FIN,
+                if (numberReceived == -1) {
+                    channel.close();
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "failed to read.");
+            }
+        } else {
+            Log.e(TAG, "SocketChannel is not valid.");
+        }
+    }
+
+    private void close() {
+        if (channel != null) {
+            try {
+                channel.close();
+            } catch (IOException e) {
+                Log.e(TAG, "failed to close.", e);
+            }
+        } else {
+            Log.e(TAG, "SocketChannel is not valid.");
+        }
+    }
+}
